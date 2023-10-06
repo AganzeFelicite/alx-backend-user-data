b09a827e0fcbacf074c5ea2c630cffedab9b48cb
AganzeFelicite <aganzefelicite@gmail.com>
2023-10-06 14:34:13 +0200

    basic auth base64 extract authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,19 @@ class BasicAuth(Auth):
     doing nothing for
     now
     """
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+        ) -> str:
+        """
+        this this is to convert to 64bytes
+        encoded data format
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[5:]
+      

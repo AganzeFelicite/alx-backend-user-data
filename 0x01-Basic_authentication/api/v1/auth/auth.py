cccde8c6a668e3e355f7e8f2db2e2d9f595913fb
AganzeFelicite <aganzefelicite@gmail.com>
2023-10-06 11:16:02 +0200

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+a class that will manage api
+authentication
+"""
+
+
+from flask import request
+
+
+class Auth:
+    """
+    this is an auth class
+    """
+    def require_auth(self,
+                     path: str,
+                     excluded_paths: List[str]
+                     ) -> bool:
+        """
+        requires auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        dealing with the authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        this returns the current loggin
+        user
+        """
+        return None

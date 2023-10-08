60fbb15bec77f9b9caf9345eacbfbac9129a5054
AganzeFelicite <aganzefelicite@gmail.com>
2023-10-08 05:23:11 +0200

    sessionauth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+this is about session management in
+python/ apis
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session auth class
+    """

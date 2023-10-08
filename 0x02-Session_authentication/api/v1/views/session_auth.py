a5c11a59a716bbdad1a7c13447efb563755ad901
AganzeFelicite <aganzefelicite@gmail.com>
2023-10-08 07:42:28 +0200

    login session /login url create and working well

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+session auth routes
+"""
+
+
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+
+
+@app_views.route("/auth_session/login", methods=['POST'], strict_slashes=False)
+def login():
+    """
+    for login a specific user
+    """
+    email = request.form.get("email")
+    passwd = request.form.get("password")
+
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if passwd is None:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+    if len(users) <= 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    if users[0].is_valid_password(passwd):
+        from api.v1.app import auth
+        session_id = auth.create_session(getattr(users[0], 'id'))
+        user = jsonify(users[0].to_json())
+        user.set_cookie("SESSION_NAME", session_id)
+        return user
+    return jsonify({"error": "wrong password"}), 401
